14675a02bff94a126ea87cf8b2524e450e292da8
Jonathan Andersson <2jonand@gmail.com>
2023-09-04 23:03:11 +0200

    updated some styling on header.css and homepage.css

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -7,16 +7,24 @@ function HomePage() {
   return (
     <div className='home-page'>
       <Header></Header>
-      <main>
-
+      <header className="hero-section">
         <div className="welcome-container">
           <h1>Välkommen till Strong n' Epic</h1>
-          
           <Link to="/booking">
-            <button>BOKA PASS</button>
+            <button><span>Boka Pass</span></button>
           </Link>
         </div>
-      
+      </header>
+      <main className="news-section">
+        <h1>Senaste Nytt</h1>
+        <article className="news-article">
+          <section className="article-left">
+          </section>
+          <section className="article-right">
+            <h2 className="article-title">Strong n' Epic öppnar nytt gym i Mjölby</h2>
+            <p className="article-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam nam nesciunt nemo reiciendis necessitatibus asperiores neque nobis nisi unde animi?</p>
+          </section>
+        </article>
       </main>
     </div>
   )
